bba8814bad5e3082ef0101a2a3e44f89814374e7
koheinthurako <captainthura99@gmail.com>
2024-03-27 01:19:43 +0630

    connect vue with bootstrap

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,50 @@
 <template>
-  <v-container>
-    <h1 class="text-primary">Hello World</h1>
+  <v-container class="mb-5">
+    <h1 class="text-dark">Calories Store</h1>
     <br>
-    {{ msg }}
-    <br>
-    <br>
-    <v-btn v-on:click="clickBtn()">Click here!</v-btn>
+
+    <v-text-field label="Enter Item Name" v-model="name"></v-text-field>
+    <v-text-field label="Enter Calories" type="number" v-model="calories"></v-text-field>
+
+    <div class="me-3">
+      <v-btn class="me-3" color="black" @click="AddNewItem">Add Item</v-btn>
+      <v-btn class="me-3" color="black" @click="DeleteItem" variant="outlined">Delete</v-btn>
+      <button class="btn btn-primary">Click Again</button>
+    </div>
+    
+    <hr class="mb-5 mt-5">
+
+    <v-table class="mt-5" theme="dark" density="compact">
+      <thead>
+        <tr>
+          <th class="text-left">
+            Name
+          </th>
+          <th class="text-left">
+            Calories
+          </th>
+          <th class="text-left">
+            Result
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- Looping -->
+        <tr
+          v-for="item in desserts"
+          :key="item.name"
+        >
+          <td>{{ item.name }}</td>
+          <td>{{ item.calories }}</td>
+          <!-- Check condition in attribute -->
+          <td v-if="item.calories<250">Good</td>
+          <td v-else>Not Good</td>
+        </tr>
+      </tbody>
+    </v-table>
+
+
+
   </v-container>
 </template>
 
@@ -15,14 +54,42 @@ export default {
   name: 'HelloWorld',
 
   data: () => ({
-    msg: "Hi how are you?"
+    name: "",
+    calories: "",
+    desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+          }
+        ]
+
   }),
 
   methods: {
-    clickBtn() {
-      console.log("Hello World!")
-      this.msg = "You clicked btn!"
-    }
+    AddNewItem() {
+      this.desserts.push({name:this.name, calories: this.calories});
+    },
+
+    DeleteItem() {
+      this.desserts.pop({name:this.name, calories: this.calories});
+    },
+
   }
 
 }
